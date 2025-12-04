eca73e31e481fd2413f5cf689d54f2e767432dea
Pivsaev <144445002+Pivsaev@users.noreply.github.com>
2025-12-04 09:10:57 +0300

    Update TFormula.h

--- TFormula.h
+++ TFormula.h
@@ -8,10 +8,15 @@ public:
 	~TFormula();
 	TFormula(const TFormula& _str);
 	TFormula operator=(const TFormula& _str);
+	TFormula();
 	int checkbrackets(int arr[], int& n);
-	char* Postfix();
-	double calculate(int& r);
 	friend std::ostream& operator<<(std::ostream& out, const TFormula& form);
+	char* Postfix();
+	friend std::istream& operator>>(std::istream& in, TFormula& form);
+	double calculate(int& r);
 };
 
 bool isdigit(char c);
+bool isop(char c);
+bool isbracket(char c);
+int priority(char op);
